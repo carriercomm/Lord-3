758d3622e8a64b13ec31984e8b5f772667e6b602
jerrywwl <jerrywwl@gmail.com>
2013-02-15 02:46:23 +0800

    Initialize project

---
+++ Build/Src/Core/CMakeFiles/libCore.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jerry/Workspace/lord/Src/Core/CoreStd.cpp" "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/CoreStd.cpp.o"
+  "/Users/jerry/Workspace/lord/Src/Core/Math/LordMath.cpp" "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/Math/LordMath.cpp.o"
+  "/Users/jerry/Workspace/lord/Src/Core/Math/Mat.cpp" "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/Math/Mat.cpp.o"
+  "/Users/jerry/Workspace/lord/Src/Core/Math/Vec2.cpp" "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/Math/Vec2.cpp.o"
+  "/Users/jerry/Workspace/lord/Src/Core/Math/Vec3.cpp" "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/Math/Vec3.cpp.o"
+  "/Users/jerry/Workspace/lord/Src/Core/Math/Vec4.cpp" "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/Math/Vec4.cpp.o"
+  "/Users/jerry/Workspace/lord/Src/Core/Memory/LordMemory.cpp" "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/Memory/LordMemory.cpp.o"
+  "/Users/jerry/Workspace/lord/Src/Core/Memory/MemAllocObj.cpp" "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/Memory/MemAllocObj.cpp.o"
+  "/Users/jerry/Workspace/lord/Src/Core/Memory/MemNedPooling.cpp" "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/Memory/MemNedPooling.cpp.o"
+  "/Users/jerry/Workspace/lord/Src/Core/Memory/MemTracker.cpp" "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/Memory/MemTracker.cpp.o"
+  "/Users/jerry/Workspace/lord/Src/Core/Util/AssertX.cpp" "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/Util/AssertX.cpp.o"
+  "/Users/jerry/Workspace/lord/Src/Core/Util/Guid.cpp" "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/Util/Guid.cpp.o"
+  "/Users/jerry/Workspace/lord/Src/Core/Util/PathUtil.cpp" "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/Util/PathUtil.cpp.o"
+  "/Users/jerry/Workspace/lord/Src/Core/Util/StringUtil.cpp" "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/Util/StringUtil.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "LORD_CORE_EXPORTS"
+  "LORD_CORE_EXPORTS"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Src/Core"
+  "../Dependencies/nedmalloc/Include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ Build/Src/Test/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jerry/Workspace/lord/Src/Test/main.cpp" "/Users/jerry/Workspace/lord/Build/Src/Test/CMakeFiles/test.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/jerry/Workspace/lord/Build/Src/Core/CMakeFiles/libCore.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Src/Core"
+  "../Dependencies/nedmalloc/Include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
